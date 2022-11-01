632f6cf78fe143007fb516651b523a9b9fdc41c7
MaximKharin <maximkharin@gmail.com>
2022-11-01 18:29:43 +0300

    finished hw

---
+++ hw6t2.py
@@ -0,0 +1,13 @@
+# Для натурального n создать словарь индекс-значение,
+# состоящий из элементов последовательности 3n + 1.
+
+
+from random import randint
+
+def get_dict(n):
+    return {x: 3 * x + 1 for x in range(1, n+1)}
+
+n = randint(5, 20)
+
+print(n)
+print(get_dict(n))
